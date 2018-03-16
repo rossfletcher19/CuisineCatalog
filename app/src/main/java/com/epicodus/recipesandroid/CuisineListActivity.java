af4e5c2f56bc95cc7d84827c0e63373b06815959
Ross Fletcher <rtbfletch@outlook.com>
2018-03-16 15:55:24 -0700

    add image and adjust listview to fit with image

--- app/src/main/java/com/epicodus/recipesandroid/CuisineListActivity.java
+++ app/src/main/java/com/epicodus/recipesandroid/CuisineListActivity.java
@@ -1,15 +1,18 @@
 package com.epicodus.recipesandroid;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class CuisineListActivity extends AppCompatActivity {
     private ListView mListView;
-    private String[] cuisines = new String[] {"cuisine1", "cuisine2", "cuisine3", "cuisine4", "cuisine5"};
+    private String[] cuisines = new String[] {"Fusion Recipes", "One-pot comfort foods", "East Coast-style meals", "West Coast-style meals"};
+    private TextView mCuisineHeadingTextView;
 
 
 
@@ -17,6 +20,10 @@ public class CuisineListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuisine_list);
+
+        mCuisineHeadingTextView = (TextView) findViewById(R.id.cuisineHeadingTextView);
+        Typeface headingFont = Typeface.createFromAsset(getAssets(), "fonts/Windsong.ttf");
+        mCuisineHeadingTextView.setTypeface(headingFont);
 
 
 
