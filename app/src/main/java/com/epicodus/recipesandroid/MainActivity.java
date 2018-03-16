52134a71373eff6ae3a81b09686365873fab73b0
Ross Fletcher <rtbfletch@outlook.com>
2018-03-16 11:06:12 -0700

    add custom fonts to assets/fonts and apply to mainAct

--- app/src/main/java/com/epicodus/recipesandroid/MainActivity.java
+++ app/src/main/java/com/epicodus/recipesandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.recipesandroid;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private TextView mMainHeadingTextView;
+    private TextView mSubHeadingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mMainHeadingTextView = (TextView) findViewById(R.id.mainHeadingTextView);
+        mSubHeadingTextView = (TextView) findViewById(R.id.subHeadingTextView);
+        Typeface headingFont = Typeface.createFromAsset(getAssets(), "fonts/Windsong.ttf");
+        mMainHeadingTextView.setTypeface(headingFont);
+        mSubHeadingTextView.setTypeface(headingFont);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
