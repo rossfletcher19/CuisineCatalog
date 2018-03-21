92ddc935f4baa05b4fd60d57bd959fc2ec73e026
Ross Fletcher <rtbfletch@outlook.com>
2018-03-21 13:10:29 -0700

    add recipeListActivity and listView with working arrayAdapter showing list of recipe IDs

--- app/src/main/java/com/epicodus/recipesandroid/CuisineListActivity.java
+++ app/src/main/java/com/epicodus/recipesandroid/CuisineListActivity.java
@@ -1,16 +1,20 @@
 package com.epicodus.recipesandroid;
 
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class CuisineListActivity extends AppCompatActivity {
-    private ListView mListView;
+    private ListView mCuisineListView;
     private String[] cuisines = new String[] {"Fusion Recipes", "One-pot comfort foods", "East Coast-style meals", "West Coast-style meals"};
     private TextView mCuisineHeadingTextView;
 
@@ -25,16 +29,21 @@ public class CuisineListActivity extends AppCompatActivity {
         Typeface headingFont = Typeface.createFromAsset(getAssets(), "fonts/Windsong.ttf");
         mCuisineHeadingTextView.setTypeface(headingFont);
 
-
-
-        mListView = (ListView) findViewById(R.id.listView);
+        mCuisineListView = (ListView) findViewById(R.id.cuisineListView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_selectable_list_item, cuisines);
-        mListView.setAdapter(adapter);
+        mCuisineListView.setAdapter(adapter);
+
+        mCuisineListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(CuisineListActivity.this, RecipesListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
-
-
-
 
 
 }
