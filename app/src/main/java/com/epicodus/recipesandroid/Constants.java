5043d62001c8ad8e014e61ebeeab69f72654130e
Ross Fletcher <rtbfletch@outlook.com>
2018-03-30 09:42:54 -0700

    remove framelayout from recipe detail fragment and convert to constraint layout and begin to customize

--- app/src/main/java/com/epicodus/recipesandroid/Constants.java
+++ app/src/main/java/com/epicodus/recipesandroid/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final String EDAMAM_KEY = BuildConfig.EDAMAM_KEY;
     public static final String EDAMAM_BASE_URL = "https://api.edamam.com/search";
     public static final String EDAMAM_QUERY_PARAMETER = "q";
+    public static final String FIREBASE_CHILD_RECIPES = "recipes";
 
 
 }
